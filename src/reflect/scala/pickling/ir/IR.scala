9f5b5b946e6c671098420e5ae944c2b5374a7caf
Heather Miller <heather.miller@epfl.ch>
2013-02-20 17:22:59 +0100

    Scala-pickling: can now pickle private state

--- src/reflect/scala/pickling/ir/IR.scala
+++ src/reflect/scala/pickling/ir/IR.scala
@@ -13,7 +13,7 @@ import scala.reflect.macros.Context
 
 
 class IRs[C <: Context with Singleton](val ctx: C) {
-  import ctx._
+  import ctx.universe._
 
   trait IR
   case class FieldIR(name: String, tpe: Type)
@@ -22,18 +22,31 @@ class IRs[C <: Context with Singleton](val ctx: C) {
   type Q = List[FieldIR]
   type C = ObjectIR
 
+  def freshName(suffix: String) = "$$expose$private$" + suffix
+
+  /* Side-effecting: generates public accessor methods for private fields */
   def fields(tp: Type): Q =
-    tp.declarations
-      .filter(sym => !sym.isMethod && sym.isTerm && (sym.asTerm.isVar || sym.asTerm.isParamAccessor)) // separate issue: minimal versus verbose PickleFormat . i.e. someone might want all concrete inherited fields in their pickle
-      .map(sym => FieldIR(sym.name.toString.trim, sym.typeSignatureIn(tp)))
-      .toList
+    (tp.declarations
+       .filter(sym => !sym.isMethod && sym.isTerm && (sym.asTerm.isVar || sym.asTerm.isParamAccessor)) // separate issue: minimal versus verbose PickleFormat . i.e. someone might want all concrete inherited fields in their pickle
+       .map { field =>
+         val fieldName = field.name.toString.trim
+         val fieldTpe  = field.typeSignatureIn(tp)
+         if (field.asTerm.getter.isPrivate) {
+           debug("processing private field " + field.name)
+           val fresh = freshName(fieldName)
+           val method = DefDef(NoMods, TermName(fresh), List(), List(), TypeTree(), Ident(field.name))
+           ctx.introduceMember(tp.typeSymbol, method)
+           FieldIR(fresh, fieldTpe)
+         } else
+           FieldIR(fieldName, fieldTpe)
+       }).toList
 
   def composition(f1: (Q, Q) => Q, f2: (C, C) => C, f3: C => List[C]) =
     (c: C) => f3(c).reverse.reduce[C](f2)
 
   val f1 = (q1: Q, q2: Q) => q1 ++ q2
 
-  val f2 = (c1: C, c2: C) => ObjectIR(c2.tpe, c1, fields(c2.tpe))
+  val f2 = (c1: C, c2: C) => ObjectIR(c2.tpe, c1, c2.fields)
 
   val f3 = (c: C) =>
     c.tpe.baseClasses
