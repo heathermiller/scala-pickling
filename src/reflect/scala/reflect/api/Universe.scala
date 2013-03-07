7ac625fe20013db6dcbe024ff4e920c30b83a769
Eugene Burmako <xeno.by@gmail.com>
2013-03-07 14:07:47 +0100

    [reflection API] brings FixedMirrorXXXCreator to scala.reflect.api.Universe

--- src/reflect/scala/reflect/api/Universe.scala
+++ src/reflect/scala/reflect/api/Universe.scala
@@ -75,6 +75,7 @@ abstract class Universe extends Symbols
                            with Printers
                            with Importers
                            with Quasiquotes
+                           with StdCreators
 {
   /** Use `refiy` to produce the abstract syntax tree representing a given Scala expression.
    *
